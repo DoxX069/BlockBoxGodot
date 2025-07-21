c35a87665140100bc575f921e9d74719c2f35acb
DoxX069 <ammonnils@gmail.com>
2025-07-21 11:03:44 +0200

    update

--- scripts/Block/block_manager.gd
+++ scripts/Block/block_manager.gd
@@ -48,22 +48,22 @@ func _process(_delta: float) -> void:
 
 # States
 
-func enter_state_new_block() ->void:
+func enter_state_new_block() -> void:
 	task_block_pos.clear()
 	build_block_pos.clear()
 	task_valid_pos = [
-		Vector3(0,1,0),
-		Vector3(-1,1,0),Vector3(1,1,0),
-		Vector3(0,1,-1),Vector3(0,1,1),
-		Vector3(-1,1,-1),Vector3(1,1,1),
-		Vector3(1,1,-1),Vector3(-1,1,1)
+		Vector3(0, 1, 0),
+		Vector3(-1, 1, 0), Vector3(1, 1, 0),
+		Vector3(0, 1, -1), Vector3(0, 1, 1),
+		Vector3(-1, 1, -1), Vector3(1, 1, 1),
+		Vector3(1, 1, -1), Vector3(-1, 1, 1)
 		]
 	build_valid_pos = [
-		Vector3(0,1,0),
-		Vector3(-1,1,0),Vector3(1,1,0),
-		Vector3(0,1,-1),Vector3(0,1,1),
-		Vector3(-1,1,-1),Vector3(1,1,1),
-		Vector3(1,1,-1),Vector3(-1,1,1)
+		Vector3(0, 1, 0),
+		Vector3(-1, 1, 0), Vector3(1, 1, 0),
+		Vector3(0, 1, -1), Vector3(0, 1, 1),
+		Vector3(-1, 1, -1), Vector3(1, 1, 1),
+		Vector3(1, 1, -1), Vector3(-1, 1, 1)
 	]
 	for i in number_of_blocks:
 		create_block_pos()
@@ -71,10 +71,10 @@ func enter_state_new_block() ->void:
 		spawn_animation()
 	
 
-func state_new_block() ->void:
+func state_new_block() -> void:
 	if swipe_up:
 		number_of_blocks += 1
-		build_timer.set_elapsed_time(build_timer.elapsed_time - clampf(reduce_time_new_block,0.5,15))
+		build_timer.set_elapsed_time(build_timer.elapsed_time - clampf(reduce_time_new_block, 0.5, 15))
 		reduce_time_new_block += 1
 		reduce_time_correct = 1
 		create_block_pos()
@@ -86,25 +86,25 @@ func state_new_block() ->void:
 		swipe_down = false
 	
 	
-func leave_state_new_block() ->void:
+func leave_state_new_block() -> void:
 	if build_timer.is_stopped:
 		build_timer.start()
 	else:
 		build_timer.resume()
 	
 	
-func enter_state_build() ->void:
+func enter_state_build() -> void:
 	make_build_block()
 	build_valid_pos = [
-		Vector3(0,1,0),
-		Vector3(-1,1,0),Vector3(1,1,0),
-		Vector3(0,1,-1),Vector3(0,1,1),
-		Vector3(-1,1,-1),Vector3(1,1,1),
-		Vector3(1,1,-1),Vector3(-1,1,1)
+		Vector3(0, 1, 0),
+		Vector3(-1, 1, 0), Vector3(1, 1, 0),
+		Vector3(0, 1, -1), Vector3(0, 1, 1),
+		Vector3(-1, 1, -1), Vector3(1, 1, 1),
+		Vector3(1, 1, -1), Vector3(-1, 1, 1)
 	]
 	
 
-func state_build() ->void:
+func state_build() -> void:
 	# Check if task- and build block positions are equal
 	if swipe_up == true:
 		make_task_block()
@@ -124,23 +124,23 @@ func state_build() ->void:
 		timer_timeout = false
 	
 
-func leave_state_build() ->void:
+func leave_state_build() -> void:
 	pass
 	
 
-func enter_state_show_task() ->void:
+func enter_state_show_task() -> void:
 	pass
 	
 
-func state_show_task() ->void:
+func state_show_task() -> void:
 	pass
 	
 	
-func leave_state_show_task() ->void:
+func leave_state_show_task() -> void:
 	pass
 
 
-func enter_state_win() ->void:
+func enter_state_win() -> void:
 	build_timer.pause()
 	build_timer.set_elapsed_time(build_timer.elapsed_time - reduce_time_correct * number_of_blocks)
 	if reduce_time_correct > 0.25:
@@ -151,67 +151,67 @@ func enter_state_win() ->void:
 	state_machine.change_state(state_new_block)
 	
 	
-func state_win() ->void:
+func state_win() -> void:
 	pass
 	
 	
-func leave_state_win() ->void:
+func leave_state_win() -> void:
 	remove_block()
 	
 	
-func enter_state_loose() ->void:
+func enter_state_loose() -> void:
 	build_timer.stop()
 	despawn_animation()
 	counter.reset_counter()
 	
 	
-func state_loose() ->void:
+func state_loose() -> void:
 	if Input.is_action_just_pressed("drag"):
-		state_machine.change_state(state_new_block)	
+		state_machine.change_state(state_new_block)
 	
 
-func leave_state_loose() ->void:
+func leave_state_loose() -> void:
 	number_of_blocks = 1
 	remove_block()
 	
 
 # Functions
 
-func custom_sorter(a,b) ->bool:
+func custom_sorter(a, b) -> bool:
 	if a < b:
 		return true
 	return false
 
 
-func make_task_block() ->void:
+func make_task_block() -> void:
 	dragging_disabled = true
 	var i := 0
-	for inst:Block in block_inst:
+	for inst: Block in block_inst:
 		inst.collision_layer = 4
 		inst.collision_mask = 4 | 2
 		inst.is_task_block = true
 		inst.is_build_block = false
 		var current_tween := get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
-		current_tween.tween_property(inst,"position",task_block_pos[i],0.2)
+		current_tween.tween_property(inst, "position", task_block_pos[i], 0.2)
 		i += 1
 	dragging_disabled = false
 		
 
-func make_build_block() ->void:
+func make_build_block() -> void:
 	dragging_disabled = true
 	var i := 0
-	for inst:Block in block_inst:
+	for inst: Block in block_inst:
 		inst.collision_layer = 1
 		inst.collision_mask = 1 | 2
 		inst.is_task_block = false
 		inst.is_build_block = true
 		var current_tween := get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_EXPO)
-		current_tween.tween_property(inst,"position",build_block_pos[i],0.2)
+		current_tween.tween_property(inst, "position", build_block_pos[i], 0.2)
 		i += 1
 	dragging_disabled = false
 	
 
-func instantiate_block() ->void:
+func instantiate_block() -> void:
 	dragging_disabled = true
 	# Task Block
 	update_task_valid_pos()
@@ -229,28 +229,28 @@ func instantiate_block() ->void:
 	dragging_disabled = false
 	
 
-func remove_block() ->void:
+func remove_block() -> void:
 	for inst: Block in block_inst:
 		inst.queue_free()
 	block_inst.clear()
 
 	
-func spawn_animation() ->void:
+func spawn_animation() -> void:
 	dragging_disabled = true
 	var current_tween := get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
-	current_tween.tween_property(current_inst.block_mesh,"scale",Vector3(1,1,1),0.15)
+	current_tween.tween_property(current_inst.block_mesh, "scale", Vector3(1, 1, 1), 0.15)
 	dragging_disabled = false
 		
 		
-func despawn_animation() ->void:
-	for inst:Block in block_inst:
+func despawn_animation() -> void:
+	for inst: Block in block_inst:
 		dragging_disabled = true
 		var current_tween := get_tree().create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
-		current_tween.tween_property(inst.block_mesh,"scale",Vector3(0,0,0),0.15)
+		current_tween.tween_property(inst.block_mesh, "scale", Vector3(0, 0, 0), 0.15)
 		dragging_disabled = false
 			
 
-func create_block_pos() ->void:
+func create_block_pos() -> void:
 	var new_task_pos: Vector3 = task_valid_pos.pick_random()
 	task_block_pos.append(new_task_pos)
 	current_inst_pos = new_task_pos
@@ -263,7 +263,7 @@ func create_block_pos() ->void:
 	update_build_valid_pos()
 	
 
-func update_task_block_pos(new_pos: Vector3, old_pos: Vector3 = Vector3()) ->void:
+func update_task_block_pos(new_pos: Vector3, old_pos: Vector3 = Vector3()) -> void:
 	if not task_block_pos.has(new_pos):
 		task_block_pos.erase(old_pos)
 		task_block_pos.append(new_pos)
@@ -273,7 +273,7 @@ func update_task_block_pos(new_pos: Vector3, old_pos: Vector3 = Vector3()) ->voi
 		print(pos)
 		
 	
-func update_build_block_pos(new_pos: Vector3, old_pos: Vector3 = Vector3()) ->void:
+func update_build_block_pos(new_pos: Vector3, old_pos: Vector3 = Vector3()) -> void:
 	if not build_block_pos.has(new_pos):
 		build_block_pos.erase(old_pos)
 		build_block_pos.append(new_pos)
@@ -283,37 +283,37 @@ func update_build_block_pos(new_pos: Vector3, old_pos: Vector3 = Vector3()) ->vo
 		print(pos)
 		
 
-func update_task_valid_pos() ->void:
+func update_task_valid_pos() -> void:
 	for pos in task_block_pos:
 		task_valid_pos.erase(pos)
 		var neighbors: Array = [
-		pos-Vector3(1,0,0), pos+Vector3(1,0,0),
-		pos+Vector3(0,1,0),
-		pos-Vector3(0,0,1), pos+Vector3(0,0,1)
+		pos - Vector3(1, 0, 0), pos + Vector3(1, 0, 0),
+		pos + Vector3(0, 1, 0),
+		pos - Vector3(0, 0, 1), pos + Vector3(0, 0, 1)
 		]
 		for n_pos in neighbors:
 			if n_pos.x >= -1 and n_pos.x <= 1 and n_pos.z >= -1 and n_pos.z <= 1:
-				var below_pos:= Vector3(n_pos.x,n_pos.y-1,n_pos.z)
+				var below_pos := Vector3(n_pos.x, n_pos.y - 1, n_pos.z)
 				if n_pos.y == 1 or below_pos in task_block_pos:
 					if n_pos not in task_block_pos:
 						task_valid_pos.append(n_pos)
 			
 
-func update_build_valid_pos() ->void:
+func update_build_valid_pos() -> void:
 	build_valid_pos = [
-		Vector3(0,1,0),
-		Vector3(-1,1,0),Vector3(1,1,0),
-		Vector3(0,1,-1),Vector3(0,1,1),
-		Vector3(-1,1,-1),Vector3(1,1,1),
-		Vector3(1,1,-1),Vector3(-1,1,1)
+		Vector3(0, 1, 0),
+		Vector3(-1, 1, 0), Vector3(1, 1, 0),
+		Vector3(0, 1, -1), Vector3(0, 1, 1),
+		Vector3(-1, 1, -1), Vector3(1, 1, 1),
+		Vector3(1, 1, -1), Vector3(-1, 1, 1)
 	]
 	for pos in build_block_pos:
 		build_valid_pos.erase(pos)
-		var neighbor: Vector3 = pos+Vector3(0,1,0)
+		var neighbor: Vector3 = pos + Vector3(0, 1, 0)
 		build_valid_pos.append(neighbor)
 			
 
-func save_data() ->void:
+func save_data() -> void:
 	var dir: DirAccess = DirAccess.open("user://")
 	dir.make_dir("saves")
 		
@@ -325,7 +325,7 @@ func save_data() ->void:
 	emit_signal("data_saved")
 	
 
-func load_data() ->void:
+func load_data() -> void:
 	var save_file = FileAccess.open("user://saves/block_data.dat", FileAccess.READ)
 	task_block_pos = save_file.get_var()
 	build_block_pos = save_file.get_var()
@@ -347,4 +347,3 @@ func _on_swipe_detector_swipe_up() -> void:
 
 func _on_swipe_detector_swipe_down() -> void:
 	swipe_down = true
-	
